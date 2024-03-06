4ec46d5afdb2bb1d6ee620235b8a59c65f9dfd06
eldrssn <maxkalimullin@gmail.com>
2024-03-06 16:30:23 +0500

    added aria-label

--- components/ui/buttons/CloseButton.tsx
+++ components/ui/buttons/CloseButton.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { TOnClick } from './types';
 
 const CloseButton: FC<TOnClick> = ({ onClick }) => (
-  <button type="button" className="w-5 h-4" onClick={onClick}>
+  <button type="button" className="w-5 h-4" aria-label="Close" onClick={onClick}>
     <span
       className="h-4 relative w-4 
       before:bg-black before:h-[2px] before:right-0 before:absolute before:top-0 before:w-5 before:rotate-45 before:translate-x-1/2
